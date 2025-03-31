e206c16dc665eda8b6d462638e0122dbda2dfb88
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2025-03-31 17:22:36 -0300

    gitignore

--- meu_app/urls.py
+++ meu_app/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('admin/', admin.site.urls),
-    path('', include('meu_app.urls')),
 ]
